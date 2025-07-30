19069a3297750ca0b2cad94bb19aa56975cdc59a
Arthur cavalcanti Granja <artcgranja@gmail.com>
2025-07-30 14:51:22 -0300

    Refactor agentic.py and main.py for improved agent functionality and vector storage integration
    - Removed agentic.py as it was replaced by a new structure in main.py.
    - Introduced a new main.py that sets up the agent with PostgreSQL vector storage using PgVector.
    - Updated project dependencies in pyproject.toml to reflect changes in libraries and versions.
    - Enhanced the agent configuration for better interaction and response handling.
    - Removed obsolete test file for vector database operations.

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-def main():
-    print("Hello, World!")
+from agno.agent import Agent, AgentKnowledge
+from agno.embedder.openai import OpenAIEmbedder
+from agno.vectordb.pgvector import PgVector, SearchType
+from agno.models.openrouter import OpenRouter
+from agno.tools.reasoning import ReasoningTools
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_url  = os.getenv("DATABASE_URL")
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+embedder = OpenAIEmbedder(id="text-embedding-3-large")
+
+vector_db = PgVector(
+    table_name="agent_knowledge",
+    schema="ai",
+    db_url=db_url,
+    search_type=SearchType.hybrid,
+    embedder=embedder,
+    auto_upgrade_schema=True
+)
+
+rag_agent = AgentKnowledge(
+    vector_db=vector_db,
+)
+
+professor_agent = Agent(
+    name="Professor nerd‑o RAG",
+    role="Especialista em Nerd‑o com busca avançada",
+    model=OpenRouter(id="google/gemini-2.5-flash", api_key=api_key, temperature=0.7),
+    knowledge=rag_agent,
+    tools=[ReasoningTools(add_instructions=True)],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+def pergunta_genérica(texto):
+    print("🤖 Resposta Genérica:")
+    professor_agent.print_response(texto, stream=True)
 
 if __name__ == "__main__":
-    main()
+    pergunta_genérica("O que é a Nerd-o?")
